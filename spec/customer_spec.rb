c82c5c6aafa0a8559650f607f08f53663572154b
Vanessa Sant'Anna <vanessa.santanna@gmail.com>
2011-11-09 20:53:27 +1100

    Revert "customer should have some money. Random up to 100 for now"
    This reverts commit 69f742713685103fc844d7f29d4609e0cd7d7c3f.

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -5,20 +5,4 @@ describe Customer do
     customer = Customer.new
     customer.money.should > 0
   end
-
-  describe "#drink" do
-    let :customer do
-      customer = Customer.new
-      customer.money = 100
-      customer
-    end
-    let :beverage do
-      beverage = mock
-      beverage.stub(:price) { 5.00 }
-    end
-    it "should use money to pay for drinks" do
-      customer.drink  beverage
-      customer.money.should == 95.00
-    end
-  end
 end
