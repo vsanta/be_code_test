4ba07787539197a40044f1ebe257534f7bc8cd74
Vanessa Sant'Anna <vanessa.santanna@gmail.com>
2011-11-09 20:54:02 +1100

    Revert "customer should have some money. Random up to 100 for now"
    This reverts commit c8e390bbbdcf1741d5b0b1615a7d1544b4ce3005.

--- lib/customer.rb
+++
@@ -1,7 +0,0 @@
-class Customer
-  attr_accessor :money
-
-  def initialize
-    @money = rand(100) +1
-  end
-end
--- spec/customer_spec.rb
+++
@@ -1,8 +0,0 @@
-require_relative "../lib/customer"
-
-describe Customer do
-  it "should have some money" do
-    customer = Customer.new
-    customer.money.should > 0
-  end
-end
